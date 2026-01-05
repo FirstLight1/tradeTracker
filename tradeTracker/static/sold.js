e743ee2fecfce23009e8761d57cccde6f137bc9f
Lukas Kamen <kamen.lukas1990@gmail.com>
2026-01-05 17:44:55 +0100

    Feat: Add dynamic class assignment for card condition in sold items display

--- tradeTracker/static/sold.js
+++ tradeTracker/static/sold.js
@@ -39,7 +39,7 @@ async function loadContent(button){
                 cardElement.innerHTML = `
                     ${renderField(card.card_name, 'text', ['card-info', 'card-name'], 'Card Name', 'card_name')}
                     ${renderField(card.card_num, 'text', ['card-info', 'card-num'], 'Card Number', 'card_num')}
-                    <p class='card-info condition' data-field="condition">${card.condition ? card.condition : 'Unknown'}</p>
+                    <p class='card-info condition ${card.condition.split(' ').join('_').toLowerCase()}' data-field="condition">${card.condition ? card.condition : 'Unknown'}</p>
                     ${renderField(card.card_price ? card.card_price + '€' : null, 'text', ['card-info', 'card-price'], 'Card Price', 'card_price')}
                     ${renderField(card.market_value ? card.market_value + '€' : null, 'text', ['card-info', 'market-value'], 'Market Value', 'market_value')}
                     ${renderField(card.sell_price ? card.sell_price + '€' : null, 'text', ['card-info', 'sell-price'], 'Sell Price', 'sell_price')}
